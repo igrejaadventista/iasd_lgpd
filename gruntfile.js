b7cb75fbcf547d6893f95efa75738839e9dbd90e
mizael isaltino <mizael.isaltino@gmail.com>
2020-09-25 08:41:27 -0300

    Ajuste final

--- gruntfile.js
+++ gruntfile.js
@@ -5,87 +5,91 @@
  */
 
 module.exports = function (grunt) {
+  "use strict";
 
-	"use strict";
+  // Force use of Unix newlines
+  grunt.util.linefeed = "\n";
 
-	// Force use of Unix newlines
-	grunt.util.linefeed = "\n";
+  RegExp.quote = function (string) {
+    return string.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
+  };
 
-	RegExp.quote = function (string) {
-		return string.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
-	};
+  // Project configuration.
+  grunt.initConfig({
+    banner:
+      "/*!\n" +
+      " * IASD LGPD Script\n" +
+      " * https://adventistas.org\n" +
+      " * Copyright 2020\n" +
+      " */\n",
 
-	// Project configuration.
-	grunt.initConfig({
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1,
+      },
+      target: {
+        files: {
+          "assets/css/styles.min.css": ["assets/css/styles.css"],
+        },
+      },
+    },
 
-		banner:
-			"/*!\n" +
-			" * IASD LGPD Script\n" +
-			" * https://adventistas.org\n" +
-			" * Copyright 2020\n" +
-			" */\n",
+    "file-creator": {
+      basic: {
+        "assets/css/styles.js": function (fs, fd, done) {
+          fs.writeSync(
+            fd,
+            'var style = "' +
+              grunt.file.read("assets/css/styles.min.css") +
+              '";'
+          );
+          done();
+        },
+      },
+    },
 
-		cssmin: {
-			options: {
-				mergeIntoShorthands: false,
-				roundingPrecision: -1
-			},
-			target: {
-				files: {
-				'assets/css/styles.min.css': ['assets/css/styles.css']
-				}
-			}
-		},
+    concat: {
+      options: {
+        stripBanners: false,
+      },
+      iasdLGPD: {
+        src: ["assets/css/styles.js", "assets/js/iasd_lgpd.js"],
+        dest: "assets/js/iasd_lgpd.concat.js",
+      },
+    },
 
-		"file-creator": {
-			basic: {
-			  "assets/css/styles.js": function(fs, fd, done) {
-				fs.writeSync(fd, "var style = \""+ grunt.file.read('assets/css/styles.min.css') +"\";");
-				done();
-			  }
-			}
-		},
+    uglify: {
+      options: {
+        report: "min",
+      },
+      iasdLGPD: {
+        options: {
+          banner: "<%= banner %>",
+        },
+        src: ["<%= concat.iasdLGPD.dest %>"],
+        dest: "dist/iasd_lgpd.min.js",
+      },
+    },
 
-		concat: {
-			options: {
-			  stripBanners: false
-			},
-			iasdLGPD: {
-			  src: [
-				'assets/css/styles.js',
-				'assets/js/iasd_lgpd.js',	  
-			  ],
-			  dest: 'assets/js/iasd_lgpd.concat.js'
-			}
-		},
+    watch: {
+      scripts: {
+        files: [
+          "assets/js/iasd_lgpd.js",
+          "assets/css/styles.js",
+          "assets/css/styles.css",
+        ],
+        tasks: ["cssmin", "file-creator", "concat", "uglify"],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+  });
 
-		uglify: {
-			options: {
-				report: "min",
-			},
-			iasdLGPD: {
-				options: {
-					banner: "<%= banner %>",
-				},
-				src: ['<%= concat.iasdLGPD.dest %>'],
-				dest: "dist/iads_lgpd.min.js",
-			},
-		},
+  // These plugins provide necessary tasks.
+  require("load-grunt-tasks")(grunt);
+  require("time-grunt")(grunt);
 
-		watch: {
-			scripts: {
-				files: ["scripts/*.js"],
-				tasks: ["uglify"],
-				options: {
-					spawn: false,
-				},
-			}
-		}
-	});
-
-	// These plugins provide necessary tasks.
-	require("load-grunt-tasks")(grunt);
-	require("time-grunt")(grunt);
-
-	grunt.registerTask("default", ["cssmin", "file-creator", "concat", "uglify"]);
+  grunt.registerTask("default", ["cssmin", "file-creator", "concat", "uglify"]);
 };
